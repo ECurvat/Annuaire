d71fe055a23645c29833dadf742622a6fb62af01
Ecurvat <curvat.elliot@outlook.com>
2022-01-14 15:33:58 +0100

    revert

--- main.c
+++ main.c
@@ -414,14 +414,9 @@ int main()
 					scanf(" %c", &validation);
 					if (validation == 'y')
 					{
-						// for (i = idclient; i <= nbligne; i++)
-						// 	tabstruct[i] = tabstruct[i + 1];
-						// nbligne--;
-						tabstruct[indices[idclient]] = tabstruct[indices[nbligne]];
+						for (i = idclient; i <= nbligne; i++)
+							tabstruct[i] = tabstruct[i + 1];
 						nbligne--;
-						tri_insertion_indirect(tabstruct, indices, nbligne, 0);
-
-
 						printf("Succes de l'operation\n");
 						printf("Il reste %d clients dans l'annuaire.\n", nbligne);
 					}
